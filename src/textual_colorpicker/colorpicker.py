e2506ee2c66ecb3100874eb6340c75c28908f003
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-04-14 20:13:26 +0100

    feat: add color preview

--- src/textual_colorpicker/colorpicker.py
+++ src/textual_colorpicker/colorpicker.py
@@ -2,7 +2,9 @@ from typing import ClassVar
 
 from textual.app import ComposeResult
 from textual.binding import Binding, BindingType
+from textual.color import Color
 from textual.containers import Container
+from textual.reactive import reactive
 from textual.widget import Widget
 from textual.widgets import Input, Static
 
@@ -16,18 +18,9 @@ class ColorPicker(Widget, can_focus=True):
     ]
 
     DEFAULT_CSS = """
-    ColorPicker > #form {
-        height: auto;
-        layout: grid;
-        grid-size: 2 4;
-        grid-rows: 4;
-        grid-columns: 3 10;
-    }
-
-    ColorPicker .label {
-        padding: 1 1;
-        text-align: right;
-        text-style: underline;
+    ColorPicker > #color-preview {
+        height: 10;
+        width: 20;
     }
     """
 
@@ -40,8 +33,30 @@ class ColorPicker(Widget, can_focus=True):
     ) -> None:
         super().__init__(name=name, id=id, classes=classes, disabled=disabled)
 
-    def compose(self) -> ComposeResult:
-        with Container(id="form"):
+    red = reactive(0)
+    green = reactive(0)
+    blue = reactive(0)
+    alpha = reactive(1.0)
+    color = reactive(Color(r=0, g=0, b=0, a=1.0))
+
+    class RgbaForm(Container):
+        DEFAULT_CSS = """
+        RgbaForm {
+            height: auto;
+            layout: grid;
+            grid-size: 2 4;
+            grid-rows: 4;
+            grid-columns: 3 10;
+        }
+
+        RgbaForm .label {
+            padding: 1 1;
+            text-align: right;
+            text-style: underline;
+        }
+        """
+
+        def compose(self) -> ComposeResult:
             yield Static("R", classes="label")
             yield Input("0", id="red", classes="input")
             yield Static("G", classes="label")
@@ -49,7 +64,38 @@ class ColorPicker(Widget, can_focus=True):
             yield Static("B", classes="label")
             yield Input("0", id="blue", classes="input")
             yield Static("A", classes="label")
-            yield Input("0", id="alpha", classes="input")
+            yield Input("1.0", id="alpha", classes="input")
+
+    def compose(self) -> ComposeResult:
+        yield self.RgbaForm()
+        yield Static(id="color-preview")
+
+    def compute_color(self) -> Color:
+        return Color(self.red, self.green, self.blue, self.alpha).clamped
+
+    def watch_color(self, color: Color) -> None:
+        self.query_one("#color-preview").styles.background = color
+
+    def on_input_changed(self, event: Input.Changed) -> None:
+        if event.input.id == "alpha":
+            try:
+                component = float(event.value)
+            except ValueError:
+                pass
+            else:
+                self.alpha = float(component)
+        else:
+            try:
+                component = int(event.value)
+            except ValueError:
+                pass
+            else:
+                if event.input.id == "red":
+                    self.red = int(component)
+                elif event.input.id == "green":
+                    self.green = int(component)
+                elif event.input.id == "blue":
+                    self.blue = int(component)
 
     def action_focus_red(self) -> None:
         self.query_one("#red", Input).focus()
