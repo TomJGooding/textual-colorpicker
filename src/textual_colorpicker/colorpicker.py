dab28c0791fb5e8a8fe7f4392f377763e72bae55
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-07-03 21:32:55 +0100

    feat: add color preview rgb or hex switcher

--- src/textual_colorpicker/colorpicker.py
+++ src/textual_colorpicker/colorpicker.py
@@ -8,8 +8,69 @@ from textual.message import Message
 from textual.reactive import var
 from textual.validation import Integer
 from textual.widget import Widget
-from textual.widgets import Input, Label, Static
+from textual.widgets import Button, ContentSwitcher, Input, Label, Static
 from textual_slider import Slider
+
+
+class ColorPreviewSwitcher(Widget):
+    DEFAULT_CSS = """
+    ColorPreviewSwitcher {
+        height: 6;
+        width: auto;
+    }
+
+    ColorPreviewSwitcher #tabs {
+        height: 3;
+    }
+
+
+    ColorPreviewSwitcher ContentSwitcher {
+        height: 3;
+    }
+
+    ColorPreviewSwitcher .color-preview {
+        height: 3;
+        width: 32;
+        content-align: center middle;
+        text-style: bold;
+    }
+    """
+    value = var(Color(r=0, g=0, b=0))
+
+    def compose(self) -> ComposeResult:
+        with Horizontal(id="tabs"):
+            yield Button("HEX", id="hex", classes="color-tab")
+            yield Button("RGB", id="rgb", classes="color-tab")
+
+        with ContentSwitcher(initial="hex"):
+            yield Static(id="hex", classes="color-preview")
+            yield Static(id="rgb", classes="color-preview")
+
+    def watch_value(self, value: Color) -> None:
+        self.update_color_previews(value)
+        self.update_color_tabs(value)
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        self.query_one(ContentSwitcher).current = event.button.id
+        self.update_color_tabs(self.value)
+
+    def update_color_previews(self, value: Color) -> None:
+        for color_preview in self.query("Static.color-preview"):
+            assert isinstance(color_preview, Static)
+            color_preview.styles.background = value
+            if color_preview.id == "hex":
+                color_preview.update(self.value.hex)
+            elif color_preview.id == "rgb":
+                color_preview.update(f"rgb{self.value.rgb}")
+
+    def update_color_tabs(self, value: Color) -> None:
+        active_color_id = self.query_one(ContentSwitcher).current
+        inactive_color = value.with_alpha(0.75)
+        for color_tab in self.query("Button.color-tab"):
+            if color_tab.id == active_color_id:
+                color_tab.styles.background = value
+            else:
+                color_tab.styles.background = inactive_color
 
 
 class RgbInput(Input):
@@ -52,6 +113,10 @@ class RgbTuner(Widget):
     RgbTuner Label {
         padding: 1;
         text-style: bold;
+    }
+
+    RgbTuner Slider {
+        width: 20;
     }
 
     RgbTuner #red-slider .slider--slider {
@@ -164,11 +229,11 @@ class ColorPicker(Widget):
         super().__init__(name=name, id=id, classes=classes, disabled=disabled)
 
     def compose(self) -> ComposeResult:
+        yield ColorPreviewSwitcher()
         yield RgbTuner()
-        yield Static(id="color-preview")
 
     def watch_value(self, value: Color) -> None:
-        self.query_one("#color-preview").styles.background = value
+        self.query_one(ColorPreviewSwitcher).value = value
 
     @on(RgbTuner.Changed)
     def on_rgb_tuner_changed(self, event: RgbTuner.Changed) -> None:
