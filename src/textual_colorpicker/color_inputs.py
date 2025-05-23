0ce535acd818454d8f112dab546a7837ac79c001
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2025-05-23 17:14:26 +0100

    feat(color inputs): add validation to rgb inputs

--- src/textual_colorpicker/color_inputs.py
+++ src/textual_colorpicker/color_inputs.py
@@ -1,14 +1,19 @@
+from __future__ import annotations
+
+from textual import on
 from textual.app import ComposeResult
 from textual.color import HSV, Color
 from textual.containers import Container, HorizontalGroup
+from textual.message import Message
 from textual.reactive import var
+from textual.validation import Integer
 from textual.widget import Widget
 from textual.widgets import Input, Label
 
 
-class RGBInputs(Widget):
+class RgbInputs(Widget):
     DEFAULT_CSS = """
-    RGBInputs {
+    RgbInputs {
         width: auto;
         height: auto;
 
@@ -28,23 +33,92 @@ class RGBInputs(Widget):
 
     color: var[Color] = var(Color(255, 0, 0), init=False)
 
+    class Changed(Message):
+        def __init__(self, rgb_inputs: RgbInputs, color: Color) -> None:
+            super().__init__()
+            self.color: Color = color
+            self.rgb_inputs: RgbInputs = rgb_inputs
+
+        @property
+        def control(self) -> RgbInputs:
+            return self.rgb_inputs
+
     def compose(self) -> ComposeResult:
         r, g, b = self.color.rgb
         with HorizontalGroup():
             yield Label("R:")
-            yield Input(str(r), id="--red-input")
+            yield Input(
+                str(r),
+                validators=Integer(0, 255),
+                id="--red-input",
+            )
         with HorizontalGroup():
             yield Label("G:")
-            yield Input(str(g), id="--green-input")
+            yield Input(
+                str(g),
+                validators=Integer(0, 255),
+                id="--green-input",
+            )
         with HorizontalGroup():
             yield Label("B:")
-            yield Input(str(b), id="--blue-input")
+            yield Input(
+                str(b),
+                validators=Integer(0, 255),
+                id="--blue-input",
+            )
 
-    def _watch_color(self) -> None:
-        r, g, b = self.color.rgb
-        self.query_one("#--red-input", Input).value = str(r)
-        self.query_one("#--green-input", Input).value = str(g)
-        self.query_one("#--blue-input", Input).value = str(b)
+    def validate_color(self, color: Color) -> Color:
+        return color.clamped
+
+    def watch_color(self) -> None:
+        self._update_all_from_color(self.color)
+
+        self.post_message(self.Changed(self, self.color))
+
+    def _update_all_from_color(self, color: Color) -> None:
+        red_input = self.query_one("#--red-input", Input)
+        green_input = self.query_one("#--green-input", Input)
+        blue_input = self.query_one("#--blue-input", Input)
+
+        clamped_color = color.clamped
+        r, g, b = clamped_color.rgb
+
+        red_input.value = str(r)
+        green_input.value = str(g)
+        blue_input.value = str(b)
+
+        # Force a re-validation of the input selections.
+        # Workaround for https://github.com/Textualize/textual/issues/5811
+        red_input.selection = red_input.selection
+        green_input.selection = green_input.selection
+        blue_input.selection = blue_input.selection
+
+    @on(Input.Blurred)
+    @on(Input.Submitted)
+    def _on_input_blurred_or_submitted(
+        self, event: Input.Blurred | Input.Submitted
+    ) -> None:
+        event.stop()
+        validation_result = event.validation_result
+        assert validation_result is not None
+        # If the value is not a number, set the input to zero.
+        if not validation_result.is_valid:
+            if any(
+                isinstance(failure, Integer.NotANumber)
+                for failure in validation_result.failures
+            ):
+                event.input.value = str(0)
+
+        # If the value is a float, round to the nearest integer.
+        r = int(float(self.query_one("#--red-input", Input).value) + 0.5)
+        g = int(float(self.query_one("#--green-input", Input).value) + 0.5)
+        b = int(float(self.query_one("#--blue-input", Input).value) + 0.5)
+
+        clamped_color = Color(r, g, b).clamped
+        # If the value is not in range, set the input to the clamped value.
+        self._update_all_from_color(clamped_color)
+
+        self.color = clamped_color
 
 
 class HSVInputs(Widget):
@@ -143,11 +217,21 @@ class ColorInputs(Container):
     }
     """
 
+    color: var[Color] = var(Color(255, 0, 0), init=False)
+
     def compose(self) -> ComposeResult:
         with HorizontalGroup():
-            yield RGBInputs()
-            yield HSVInputs()
-        yield HexInput()
+            yield RgbInputs()
+            yield HSVInputs(disabled=True)
+        yield HexInput(disabled=True)
+
+    def watch_color(self) -> None:
+        hex_value = self.color.hex.lower().lstrip("#")
+        self.query_one(RgbInputs).color = self.color
+        self.query_one(HexInput).value = hex_value
+
+    def _on_rgb_inputs_changed(self, event: RgbInputs.Changed) -> None:
+        self.color = event.color
 
 
 if __name__ == "__main__":
--- src/textual_colorpicker/color_picker.py
+++ src/textual_colorpicker/color_picker.py
@@ -5,7 +5,7 @@ from textual.reactive import var
 from textual.widget import Widget
 
 from textual_colorpicker._color_hsv import _color_from_hsv
-from textual_colorpicker.color_inputs import ColorInputs, HexInput, HSVInputs, RGBInputs
+from textual_colorpicker.color_inputs import ColorInputs, HexInput, HSVInputs, RgbInputs
 from textual_colorpicker.color_preview import ColorPreview
 from textual_colorpicker.hue_picker import HuePicker
 from textual_colorpicker.saturation_value_picker import SaturationValuePicker
@@ -64,7 +64,7 @@ class ColorPicker(Widget):
         self.query_one(ColorPreview).color = color
 
         self.query_one(HSVInputs).hsv = hsv
-        self.query_one(RGBInputs).color = color
+        self.query_one(RgbInputs).color = color
         self.query_one(HexInput).value = color.hex.lstrip("#")
 
     def _on_saturation_value_picker_changed(
