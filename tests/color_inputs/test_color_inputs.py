cf1098b9c92e931a5b4c00bd33e6d6ad65b304eb
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2025-05-30 09:53:52 +0100

    test(color inputs): add color inputs tests

--- tests/color_inputs/test_color_inputs.py
+++ tests/color_inputs/test_color_inputs.py
@@ -40,3 +40,52 @@ async def test_changing_color_updates_all_inputs() -> None:
 
         hex_input = color_inputs.query_one(HexInput)
         assert hex_input.value == "#00FFFF"
+
+
+async def test_updating_rgb_inputs_changes_color() -> None:
+    app = ColorInputsApp()
+    async with app.run_test() as pilot:
+        color_inputs = pilot.app.query_one(ColorInputs)
+
+        rgb_inputs = color_inputs.query_one(RgbInputs)
+        rgb_inputs.color = Color(0, 255, 255)
+        await pilot.pause()
+
+        assert color_inputs.color == Color(0, 255, 255)
+
+
+async def test_updating_hsv_inputs_changes_color() -> None:
+    app = ColorInputsApp()
+    async with app.run_test() as pilot:
+        color_inputs = pilot.app.query_one(ColorInputs)
+
+        hsv_inputs = color_inputs.query_one(HsvInputs)
+        hsv_inputs.hsv = HSV(0.5, 1.0, 1.0)
+        await pilot.pause()
+
+        assert color_inputs.color == Color(0, 255, 255)
+
+
+async def test_updating_hex_input_changes_color() -> None:
+    app = ColorInputsApp()
+    async with app.run_test() as pilot:
+        color_inputs = pilot.app.query_one(ColorInputs)
+
+        hex_input = color_inputs.query_one(HexInput)
+        hex_input.value = "#00FFFF"
+        await pilot.pause()
+
+        assert color_inputs.color == Color(0, 255, 255)
+
+
+async def test_changed_color_posts_message() -> None:
+    app = ColorInputsApp()
+    async with app.run_test() as pilot:
+        color_inputs = pilot.app.query_one(ColorInputs)
+        expected_messages: list[str] = []
+        assert app.messages == expected_messages
+
+        color_inputs.color = Color(0, 255, 255)
+        await pilot.pause()
+        expected_messages.append("Changed")
+        assert app.messages == expected_messages
